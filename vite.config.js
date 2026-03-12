9e5837603561ca3e2610fecbc08aac9f5949670b
mbcx7sh3 <mbcx7sh3@E-LOSX6JP7Q.local>
2026-03-12 20:38:43 +0000

    updated deployment options

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
 const isGitHubPagesBuild = process.env.GITHUB_PAGES === 'true' || process.env.GITHUB_PAGES === '1'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: isGitHubPagesBuild && repoName ? `/${repoName}/` : '/',
+  base: "/neon_ceefax",
 })
